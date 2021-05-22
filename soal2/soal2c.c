303aed2c281c4d595c171deb8604e27b6e13971b
Heavenhpf <62087953+HeavenPutra208@users.noreply.github.com>
2021-05-22 20:08:04 +0700

    Revisi soal 2c

--- soal2/soal2c.c
+++ soal2/soal2c.c
@@ -1,56 +1,62 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <sys/shm.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/ipc.h>
+#include <stdio.h>
+#include <wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
 
-static void
-multiPipes(char ***command) //loop membuat pipes
+// var. global
+int p[2];
+int p2[2];
+
+#define die(x) do {fprintf(stderr, "%s\n", x); exit(EXIT_FAILURE);} while (0);
+
+int main()
 {
-	int fd[2];
-    int fe = 0;				//backup
-	pid_t pid;
+	pipe(p); // inisialisasi pipe p
 
-	while (*command != NULL) {
-		pipe(fd);				//membagi source pipes
-		if (pid == 0) {    //jika pid 0
-			dup2(fe, 0);
-			if (*(command+1) != NULL) {
-
-				dup2(fd[1], 1);
-
-			}
-			close(fd[0]);
-			execvp((*command)[0], *command); //memasukkan
-			exit(1);
-		}
-        
-        if ((pid = fork()) == -1) { //jika pipes salah
-			perror("fork");         //atau eror
-			exit(1);                //exit status 1
-		}
-
-		else { //menambah cmd line
-			wait(NULL); 		//mengambil child
-			close(fd[1]);
-			fe = fd[0];
-			command++; //increment jumlah cmd line
-		}
+	if(pipe(p) == -1) { // jika p salah
+		die("pipe");
 	}
-}
 
-int main(int argc, char *argv[])
-{
-	char *ps[] = {"ps", "aux", NULL}; //pipe s1
-	char *sort[] = {"sort", "-nrk", "3,3", NULL}; //pipe s2
-	char *head[] = {"head", "-5", NULL}; //pipe s3
-	char **cmd[] = {ps, sort, head, NULL}; //binding cmd pipes
+    if (fork() == 0) // fork1
+    {
+        close(p[0]);
+        dup2(p[1], 1);
+        close(p[1]);
+        execlp("ps", "ps", "aux", NULL); // cmd 1
+    }
 
-	multiPipes(cmd);//run cmd pakai fungsi multiPipes
-	return (0);
+    pipe(p2); // inisialisasi pipe p2
+
+	if(pipe(p2) == -1) { // jika p2 salah
+		die("pipe");
+	}
+
+    if (fork() == 0) // fork2
+    {
+        close(p[1]);
+        dup2(p[0], 0);
+        close(p[0]);
+
+        close(p2[0]);
+        dup2(p2[1], 1);
+        close(p2[1]);
+        execlp("sort", "sort", "-nrk", "3,3", NULL); // cmd 2
+    }
+
+	close(p[0]); // close pipe p[0]
+    close(p[1]); // close pipe p[1]
+
+    if (fork() == 0)
+    {
+        close(p2[1]);
+        dup2(p2[0], 0);
+        close(p2[0]);
+        execlp("head", "head", "-5", NULL); // cmd 3
+    }
+
+    close(p2[0]); // close pipe p2[0]
+    close(p2[1]); // clode pipe p2[1]
+
+	return(0);
 }
